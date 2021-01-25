44ae415809d92ef7a5c1cda935a8d5c5f59be93f
mateusfg7 <mateusfelipefg77@gmail.com>
2021-01-25 11:42:04 -0300

    use tailwind class on Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,7 @@
-const Layout: React.FC = () => {
-  return <div></div>;
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div className='max-w-5xl mx-auto px-4 sm:px-6 md:px-8'>{children}</div>
+  );
 };
 
 export default Layout;
